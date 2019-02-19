223dbf92571d0d924af120ea35e9567d819e273d
Austin Giberti <austingibertigit config --global user.email austingiberti@gmail.com>
2019-02-19 09:17:45 -0600

    Implement the watchform function for brewery search

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-API_KEY = "5j2OFsyfrQhAQYEZt5F8IvnB5eiDVc0HecM9VEpsr9f5eqtWqhov9p6jsIxHoDhdJNYpuO4Lukeu03zZ1rVaZJyMtwnMjzQvUQK7YsJaJzd3SLJiuCUtgs4NFV5mXHYx"
+'use strict';
+const review_key = "5j2OFsyfrQhAQYEZt5F8IvnB5eiDVc0HecM9VEpsr9f5eqtWqhov9p6jsIxHoDhdJNYpuO4Lukeu03zZ1rVaZJyMtwnMjzQvUQK7YsJaJzd3SLJiuCUtgs4NFV5mXHYx";
+const review_url = "https://api.yelp.com/v3";
+const brewery_url = "https://api.openbrewerydb.org/breweries";
 
+// watchform 
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const citySearch = $('js-search-bar').val();
+        getBreweryResults(citySearch);
+    })
+}
 
+// format query params
+
+// get results from brewery API
+
+// display the results
+
+// click function for reviews
+
+// display review results
+
+// handler 
