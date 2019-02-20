ab8b11d8981b76c466c2156c118db18423b22e9d
Austin Giberti <austingibertigit config --global user.email austingiberti@gmail.com>
2019-02-20 11:45:19 -0600

    Implemented calling the brewery api, getting results, displaying results

--- index.js
+++ index.js
@@ -4,22 +4,57 @@ const review_url = "https://api.yelp.com/v3";
 const brewery_url = "https://api.openbrewerydb.org/breweries";
 
 // watchform 
-function watchForm() {
+function watchFormBreweries() {
     $('form').submit(event => {
         event.preventDefault();
-        const citySearch = $('js-search-bar').val();
+        const citySearch = $('#js-search-bar').val();
         getBreweryResults(citySearch);
     })
 }
-
 // format query params
-
+function formatQueryParams(breweryParams) {
+    const queryItems = Object.keys(breweryParams)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(breweryParams[key])}`)
+    return queryItems.join('&');
+}
 // get results from brewery API
-
-// display the results
-
+function getBreweryResults(query) {
+    const breweryParams = {
+        by_city : query,
+    };
+    const queryString = formatQueryParams(breweryParams);
+    const url = brewery_url + '?' + queryString;
+    fetch(url) 
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => {
+            console.log(responseJson)
+            displayBreweryResults(responseJson)
+        })
+        .catch(err => {
+            $('#js-error-message').text('something went wrong');
+        });
+}
+//  display the results
+function displayBreweryResults(responseJson) {
+    $('.brewery-list').empty();
+    for (let i = 0; i < responseJson.length; i++) 
+        $('.brewery-list').append
+            (`<li>
+                <h3>${responseJson[i].name}</h3>
+                <p>${responseJson[i].street}</br>${responseJson[i].city}, ${responseJson[i].state}</p>
+                <a href="${responseJson[i].website_url}" target="_blank">Link to website</a>
+            </li>`)
+}
 // click function for reviews
 
-// display review results
+// get reviews
+
+// display the results for the reviews of brewery
 
 // handler 
+$(watchForm);
