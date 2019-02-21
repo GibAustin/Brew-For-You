fa41075226a244e0dd20b0738e4c9c2b851589b0
Austin Giberti <austingibertigit config --global user.email austingiberti@gmail.com>
2019-02-21 00:17:40 -0600

    Remove unnecessary code and reviews portion of web app

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 'use strict';
-const review_key = "5j2OFsyfrQhAQYEZt5F8IvnB5eiDVc0HecM9VEpsr9f5eqtWqhov9p6jsIxHoDhdJNYpuO4Lukeu03zZ1rVaZJyMtwnMjzQvUQK7YsJaJzd3SLJiuCUtgs4NFV5mXHYx";
-const review_url = "https://api.yelp.com/v3";
 const brewery_url = "https://api.openbrewerydb.org/breweries";
-
+const store = {
+    selectedBrewery: []
+}
 // watchform 
 function watchFormBreweries() {
     $('form').submit(event => {
         event.preventDefault();
-        const citySearch = $('#js-search-bar').val();
-        getBreweryResults(citySearch);
+        const city = $('#js-search-bar').val();
+        getBreweryResults(city);
+        $('.brewery-list').removeClass('hidden');
     })
 }
-// format query params
-function formatQueryParams(breweryParams) {
-    const queryItems = Object.keys(breweryParams)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(breweryParams[key])}`)
-    return queryItems.join('&');
-}
 // get results from brewery API
-function getBreweryResults(query) {
-    const breweryParams = {
-        by_city : query,
-    };
-    const queryString = formatQueryParams(breweryParams);
-    const url = brewery_url + '?' + queryString;
-    fetch(url) 
+function getBreweryResults(city) {
+    return fetch (`${brewery_url}?by_city=${encodeURIComponent(city)}`)
         .then(response => {
             if (response.ok) {
                 return response.json();
@@ -32,6 +22,7 @@ function getBreweryResults(query) {
             throw new Error(response.statusText);
         })
         .then(responseJson => {
+            store.selectedBrewery = responseJson;
             console.log(responseJson)
             displayBreweryResults(responseJson)
         })
@@ -39,22 +30,16 @@ function getBreweryResults(query) {
             $('#js-error-message').text('something went wrong');
         });
 }
-//  display the results
+//display the results
 function displayBreweryResults(responseJson) {
     $('.brewery-list').empty();
-    for (let i = 0; i < responseJson.length; i++) 
+    for (let i = 0; i < responseJson.length; i++)
         $('.brewery-list').append
-            (`<li>
+            (`<li class="review-link" data-longitude="${responseJson[i].longitude}" data-latitude="${responseJson[i].latitude}">
                 <h3>${responseJson[i].name}</h3>
                 <p>${responseJson[i].street}</br>${responseJson[i].city}, ${responseJson[i].state}</p>
                 <a href="${responseJson[i].website_url}" target="_blank">Link to website</a>
             </li>`)
 }
-// click function for reviews
-
-// get reviews
-
-// display the results for the reviews of brewery
-
 // handler 
-$(watchForm);
+watchFormBreweries();
