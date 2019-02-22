3117986454481b361554f380b8a7181d2c988272
Austin Giberti <austingibertigit config --global user.email austingiberti@gmail.com>
2019-02-22 11:24:08 -0600

    Add styling to mobile landing page

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const brewery_url = "https://api.openbrewerydb.org/breweries";
 const store = {
     selectedBrewery: []
 }
+
+var searchEx = [ 'Want some suggestions?', 'Chicago', 'San Fransisco', 'Austin', 'New York', 'Nashville', 'Atlanta', 'Portland', 'Denver', 'Raleigh', 'Detroit', 'San Diego', ];
+setInterval(function() {
+  $("input#js-search-bar").attr("placeholder", searchEx[searchEx.push(searchEx.shift())-1]);
+}, 3000);
+
 // watchform 
 function watchFormBreweries() {
     $('form').submit(event => {
@@ -14,7 +20,7 @@ function watchFormBreweries() {
 }
 // get results from brewery API
 function getBreweryResults(city) {
-    return fetch (`${brewery_url}?by_city=${encodeURIComponent(city)}`)
+    return fetch(`${brewery_url}?by_city=${encodeURIComponent(city)}`)
         .then(response => {
             if (response.ok) {
                 return response.json();
@@ -33,6 +39,7 @@ function getBreweryResults(city) {
 //display the results
 function displayBreweryResults(responseJson) {
     $('.brewery-list').empty();
+    $('.error-message').empty();
     for (let i = 0; i < responseJson.length; i++)
         $('.brewery-list').append
             (`<li class="review-link" data-longitude="${responseJson[i].longitude}" data-latitude="${responseJson[i].latitude}">
@@ -40,6 +47,9 @@ function displayBreweryResults(responseJson) {
                 <p>${responseJson[i].street}</br>${responseJson[i].city}, ${responseJson[i].state}</p>
                 <a href="${responseJson[i].website_url}" target="_blank">Link to website</a>
             </li>`)
+    if (responseJson.length === 0) {
+        $('#js-error-message').text('No results. Try again.');
+    }
 }
 // handler 
 watchFormBreweries();
