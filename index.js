6dc7f403ca1ba3b6a0070570ae6ffcf2fb311cf7
Austin Giberti <austingibertigit config --global user.email austingiberti@gmail.com>
2019-02-23 17:20:27 -0600

    Minor adjustments to styling, touched up code, added screenshots for readme

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 const brewery_url = "https://api.openbrewerydb.org/breweries";
 // Searchbar animation
-var searchEx = ['Want some suggestions?', 'Chicago', 'San Francisco', 'Austin', 'New York', 'Nashville', 'Atlanta', 'Portland', 'Denver', 'Raleigh', 'Detroit', 'San Diego',];
+var searchExamples = ['Want some suggestions?', 'Chicago', 'San Francisco', 'Austin', 'New York', 'Nashville', 'Atlanta', 'Portland', 'Denver', 'Raleigh', 'Detroit', 'San Diego',];
 setInterval(function () {
-    $("input#js-search-bar").attr("placeholder", searchEx[searchEx.push(searchEx.shift()) - 1]);
+    $("input#js-search-bar").attr("placeholder", searchExamples[searchExamples.push(searchExamples.shift()) - 1]);
 }, 3000);
 // event listener for when the user searches a specific city
 function watchFormBreweries() {
@@ -17,8 +17,8 @@ function watchFormBreweries() {
             getBreweryResults(city);
             $('.brewery-list').removeClass('hidden');
         }
-    })
-}
+    });
+};
 // fetch the results for search query from brewery_url
 function getBreweryResults(city) {
     return fetch(`${brewery_url}?by_city=${encodeURIComponent(city)}`)
@@ -29,13 +29,12 @@ function getBreweryResults(city) {
             throw new Error(response.statusText);
         })
         .then(responseJson => {
-            console.log(responseJson)
             displayBreweryResults(responseJson)
         })
         .catch(err => {
             $('#js-error-message').text('something went wrong');
         });
-}
+};
 // display the search results for breweries
 function displayBreweryResults(responseJson) {
     $('.brewery-list').empty();
@@ -47,9 +46,9 @@ function displayBreweryResults(responseJson) {
                 <h3>${responseJson[i].name}</h3>
                 <p>${responseJson[i].street.length > 0 ? `${responseJson[i].street} </br>` : ''}${responseJson[i].city}, ${responseJson[i].state}</p>
                 </a>
-            </li>`)
+            </li>`);
     if (responseJson.length === 0) {
         $('#js-error-message').text('No results. Try again.');
     }
-}
+};
 watchFormBreweries();
